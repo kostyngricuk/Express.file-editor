f27165b2d2b048f7fabc4dcd46b11d1a30fc1a46
Kanstantsin Hrytsuk <kanstantsin.hrytsuk@instinctools.com>
2022-08-05 18:05:16 +0300

    Connect XLSX module

--- routes/type/xlsx.js
+++ routes/type/xlsx.js
@@ -1,19 +1,55 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const multer = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage }).single("upload_file")
+const XLSX = require('xlsx')
 
 /* GET xlsx */
 router.route('/')
-    .get(function(req, res) {
-        res.end('GET');
-    })
-    .post(function(req, res) {
-        res.end('POST')
-    })
-    .put(function(req, res) {
-        res.end('PUT')
-    })
-    .delete(function(req, res) {
-        res.end('DELETE')
+    .post((req, res) => {
+        upload(req, res, (err) => {
+            if (err) {
+                res.status(400).send("Something went wrong!");
+            }
+
+            workbook = XLSX.read(req.file.buffer, {
+                type: 'buffer',
+            })
+
+            let worksheet = workbook.Sheets[workbook.SheetNames[0]]
+
+            let worksheetJson = XLSX.utils.sheet_to_json(worksheet)
+
+            let worksheetString = JSON.stringify(worksheetJson);
+
+            // UPDATE DATA
+            let index = '001'
+            let day = '2'
+            let month = 'августа'
+            let year = '2022'
+            let number_plate = 'AS-2312'
+            let number_garage = '4Д'
+            worksheetString = worksheetString.replaceAll('#index#', index)
+            worksheetString = worksheetString.replaceAll('#day#', day)
+            worksheetString = worksheetString.replaceAll('#month#', month)
+            worksheetString = worksheetString.replaceAll('#year#', year)
+            worksheetString = worksheetString.replaceAll('#number_plate#', number_plate)
+            worksheetString = worksheetString.replaceAll('#number_garage#', number_garage)
+            // ---
+
+            let resJson = JSON.parse(worksheetString)
+
+            res.send(resJson);
+
+
+            // const buf = XLSX.write(workbook, { type:"buffer", bookType:"xlsx" });
+            // res.statusCode = 200;
+            // res.setHeader('Content-Disposition', 'attachment; filename="result.xlsx"');
+            // res.setHeader('Content-Type', 'application/vnd.ms-excel');
+            // res.end(buf);
+        })
     })
 
 module.exports = router;
