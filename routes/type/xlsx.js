4e4ec6d77135bc9cb72cf270f9aa0f4fbc630dc4
Kanstantsin Hrytsuk <kanstantsin.hrytsuk@instinctools.com>
2022-08-11 09:59:04 +0300

    Update download files methods

--- routes/type/xlsx.js
+++ routes/type/xlsx.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 const path = require('path')
 const formidable = require('formidable')
 const XLSX = require('xlsx')
+const logger = require('npmlog');
 
 const resault_folder = 'uploads'
 
@@ -15,7 +16,7 @@ router.route('/')
         let use_strict = false
         new formidable.IncomingForm().parse(req)
             .on('field', (name, field) => {
-                console.log(`Field name: ${name}, Field value: ${field}`)
+                // logger.info('XLSX', `Field name: ${name}, Field value: ${field}`)
                 switch (name) {
                     case 'number_start':
                         number_start = field?field:1
@@ -45,34 +46,46 @@ router.route('/')
                 }
             })
             .on('aborted', () => {
-                console.error('Request aborted by the user')
+                logger.warn('XLSX', 'Request aborted by the user')
             })
             .on('error', (err) => {
-                console.error('Error', err)
+                logger.error('XLSX', 'Error: %j', err)
                 throw err
             })
             .on('end', () => {
-                let data_json = JSON.parse(fs.readFileSync(data_path))
+                try {
+                    let data_json = JSON.parse(fs.readFileSync(data_path))
 
-                while (date_start <= date_end) {
-                    let resFilePath = generateFileFolders(date_start, use_strict)
+                    let date_loop = date_start
+                    while (date_loop <= date_end) {
+                        let file_save_path = generateFileFolders(date_loop, use_strict)
+                        let file_name = 'file_name'
 
-                    console.log(resFilePath)
+                        if (!use_strict) {
+                            file_name += '-' + date_loop.toDateString().replaceAll(' ', '_')
+                        }
+                        // ...
+                        renderWorksheet(file_save_path, file_name, template_path, data_json)
 
-                    let next_day = date_start.setDate(date_start.getDate() + 1);
-                    date_start = new Date(next_day);
+                        logger.info('XLSX', 'Loading ...')
+
+                        date_loop = new Date(date_loop.setDate(date_loop.getDate() + 1));
+                    }
+
+                    logger.info('XLSX', 'Processed successfully!')
+                    res.statusCode = 200;
+                    res.end('Processed successfully!');
+                } catch (error) {
+                    logger.error('XLSX', 'Processed with errors: %j', error)
+                    res.statusCode = 500;
+                    res.end('Processed with errors!');
                 }
-
-                // let buffer = renderWorksheet(template_path, data_json)res.statusCode = 200;
-                // res.setHeader('Content-Disposition', 'attachment; filename="result.xlsx"');
-                // res.setHeader('Content-Type', 'application/vnd.ms-excel');
-                // res.end(buffer);
             })
     })
 
 function generateFileFolders(dateStart, useStrict = false) {
     // current day folder
-    let res_folder = path.join(resault_folder, new Date().toDateString())
+    let res_folder = path.join(resault_folder, new Date().toDateString().replaceAll(' ', '_'))
     if ( !fs.existsSync(res_folder) ) {
         fs.mkdirSync(res_folder)
     }
@@ -99,34 +112,36 @@ function generateFileFolders(dateStart, useStrict = false) {
     return res_folder
 }
 
-function renderWorksheet(templatePath, data) {
-    let wb = XLSX.readFileSync(templatePath)
+function renderWorksheet(fileSavePath, fileName, templatePath, data) {
+    fileSavePath = path.join(fileSavePath, fileName+'.xlsx')
+
+    let wb = XLSX.readFileSync(templatePath, {
+        cellNF: true,
+        cellStyles: true
+    })
     let ws = wb.Sheets[wb.SheetNames[0]]
     let ref = XLSX.utils.decode_range(ws["!ref"]);
 
-    var wscols = new Array();
-
-    for (var r = ref.s.r; r <= ref.e.r; r++) {
-        for (var c = ref.s.c; c <= ref.e.c; c++) {
-            if (r === ref.s.r) {
-                wscols.push({ wpx: 9 })
-            }
-            
-            let cell_name = XLSX.utils.encode_cell({ c: c, r: r });
-            if (ws[cell_name]) {
-                let current_value = ws[cell_name].v.toString()
-                let update_value = updateCellValue(current_value)
-                ws[cell_name] = { t: 's', v: update_value };
+    for (var R = ref.s.r; R <= ref.e.r; R++) {
+        for (var C = ref.s.c; C <= ref.e.c; C++) {
+            let cell_name = XLSX.utils.encode_cell({ c: C, r: R });
+            if (ws[cell_name] && ws[cell_name].t == 's') {
+                Object.keys(ws[cell_name]).forEach(key => {
+                    let current_value = ws[cell_name][key].toString()
+                    ws[cell_name][key] = updateCellValue(current_value);
+                });
             } else {
                 continue;
             }
         }
     }
 
-    ws['!cols'] = wscols;
     ws["!ref"] = XLSX.utils.encode_range(ref);
 
-    return XLSX.write(wb, { type: "buffer", bookType: "xlsx" });
+    return XLSX.writeFile(wb, fileSavePath, {
+        themeXLSX: true, 
+        compression: true
+    })
 }
 
 function updateCellValue(currentValue) {
